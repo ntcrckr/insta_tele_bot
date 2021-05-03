c9e2ec4b6d0d14793bc9431e45f3a941b3a06fd6
ntcrckr <alexei.pozdnyakov@mail.ru>
2021-05-03 18:35:12 +0300

    new buildpacks for heroku

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-driver.get("https://www.google.com")
-print(driver.page_source)
+browser.get("https://www.google.com")
+print(browser.title)
+browser.close()
